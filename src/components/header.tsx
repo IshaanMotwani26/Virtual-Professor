3b6dec28739f2d56e4632eca5e0334c47ff2fc31
kyracoding <pixelesc.com@gmail.com>
2025-09-20 16:27:40 -0400

    Refactor Header component layout and styling for improved responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,17 @@
 export default function Header() {
-    return (
-        <div className="flex py-4 px-8 h-20 w-full bg-slate-100 shadow">
-            <div className="h-full aspect-square flex items-center rounded-xl bg-blue-400">
-                <img src="/logo.png" alt="logo" className="p-1"/>
-            </div>
-            <div className="pl-4 items-center flex text-2xl font-bold">
-                VirtualProfessor
-            </div>
+  return (
+    <div className="flex justify-between items-center py-2 px-8 h-16 w-full shadow">
+      <a href="/" className="flex w-fit cursor-pointer">
+        <div className="h-12 w-12 aspect-square flex items-center rounded-xl bg-blue-400">
+          <img src="/logo.png" alt="logo" className="p-1" />
         </div>
-    )
+        <div className="pl-4 flex items-center text-2xl font-bold">
+          VirtualProfessor
+        </div>
+      </a>
+      <a className="flex cursor-pointer bg-green-400 hover:bg-green-500 transition duration-300 rounded-lg h-full items-center p-2">
+        Login
+      </a>
+    </div>
+  )
 }
